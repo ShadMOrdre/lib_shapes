92c9fd5c515533c6531771bb91c97cbd1f32ad03
Shad MOrdre <shadmordre@gmail.com>
2019-09-02 16:04:55 -0700

    Add files via upload

--- init.lua
+++ init.lua
@@ -29,128 +29,207 @@
 --All graphics are original works by ShadMOrdre@gmail.com, and are licensed CC-BY-SA.
 
 
+
+
 lib_shapes = {}
+lib_shapes.name = "lib_shapes"
 
-local S
-if minetest.get_modpath("intllib") then
-    S = intllib.Getter()
-else
-    S = function(s,a,...)a={a,...}return s:gsub("@(%d+)",function(n)return a[tonumber(n)]end)end
-end
+lib_shapes.ver_max = 0
+lib_shapes.ver_min = 0
+lib_shapes.ver_rev = 1
+lib_shapes.path_mod = minetest.get_modpath(minetest.get_current_modname())
+lib_shapes.path_world = minetest.get_worldpath()
 
-lib_shapes.registered_shapes = {}
 
-local modpath = minetest.get_modpath("lib_shapes")
-local applicable_nodes = {}
-lib_shapes.registered_doors = {}
 
-lib_shapes.shapes_created = 0
-lib_shapes.nodes_used = 0
-lib_shapes.nodes_created = 0
 
-dofile(modpath.."/craftitems.lua")
-dofile(modpath.."/utility.lua")
+	-- Intllib
 
-function lib_shapes.get_shapes()
+	local S
 
-	--local shape_list = {}
-	
-	for shape, def in pairs(registered_shapes) do
+	local NS
 
-		lib_shapes.shapes_created = lib_shapes.shapes_created + 1
+	S, NS = dofile(lib_shapes.path_mod.."/intllib.lua")
 
-		minetest.debug(lib_shapes.shapes_created)
-		
+	lib_shapes.intllib = S
+
+minetest.log(S("[MOD] lib_shapes:  Loading..."))
+
+
+
+	lib_shapes.lib_shapes_enable = minetest.settings:get_bool("lib_shapes_enable") or true
+
+	if lib_shapes.lib_shapes_enable == false then
+		return
 	end
 
-end
 
+	lib_shapes.registered_shapes = {}
 
-lib_shapes.register_shape = function(shape_name, shape_def)
+	lib_shapes.applicable_nodes = {}
+	lib_shapes.registered_doors = {}
 
-	local new_shape_name = ""
-	local new_shape_def = {}
+	lib_shapes.shapes_created = 0
+	lib_shapes.nodes_used = 0
+	lib_shapes.nodes_created = 0
 
-	new_shape_name = "shape_" .. shape_name
-	new_shape_def = {
-		description = shape_def.description .. " Shape",
-		drawtype = "nodebox",
-		tiles = {
-			"dk_grey_noise.png^align_grid_ypos.png",
-			"dk_grey_noise.png^align_grid_yneg.png",
-			"dk_grey_noise.png^align_grid_xpos.png",
-			"dk_grey_noise.png^align_grid_xneg.png",
-			"dk_grey_noise.png^align_grid_zpos.png",
-			"dk_grey_noise.png^align_grid_zneg.png",
-		},
-		paramtype = "light",
-		paramtype2 = "facedir",
-		connects_to = { "group:lib_shapes_library" },
-		is_ground_content = false,
-		walkable = true,
-		climbable = false,
-		buildable_to = false,
-		groups = { oddly_breakable_by_hand = 3, lib_shapes_library = 3 },
-		node_box = shape_def.node_box,
-		selection_box = shape_def.selection_box,
-		collision_box = shape_def.collision_box,
+	local materials = {"stone", "metal", "tree", "wood", "glass" }
 
-		on_place = function(itemstack, placer, pointed_thing)
-			if pointed_thing.type ~= "node" then
-				return itemstack
-			end
-
-			local p0 = pointed_thing.under
-			local p1 = pointed_thing.above
-			local param2 = 0
-
-			local placer_pos = placer:getpos()
-			if placer_pos then
-				local dir = {
-					x = p1.x - placer_pos.x,
-					y = p1.y - placer_pos.y,
-					z = p1.z - placer_pos.z
-				}
-				param2 = minetest.dir_to_facedir(dir)
-			end
-
-			if p0.y-1 == p1.y then
-				param2 = param2 + 20
-				if param2 == 21 then
-					param2 = 23
-				elseif param2 == 23 then
-					param2 = 21
-				end
-			end
-
-			return minetest.item_place(itemstack, placer, pointed_thing, param2)
-		end,
+	local mat_list = {
+		mat_name = "",
+		shape_set = ""
 	}
 
 
-	minetest.register_node("lib_shapes:" .. new_shape_name, new_shape_def)
-	minetest.register_craft({
-		output = new_shape_name .. " 1",
-		recipe = {
-			{ 'lib_shapes:blueprint', 'lib_shapes:blueprint', 'lib_shapes:blueprint' },
-			{ '', 'lib_shapes:blueprint', ''},
-			{ '', '', ''},
-		}
-	})
+	dofile(lib_shapes.path_mod.."/craftitems.lua")
+
+	dofile(lib_shapes.path_mod.."/utility.lua")
+
+		--dofile(lib_shapes.path_mod.."/node_list.lua")
+
+
+
+	function lib_shapes.get_shapes()
+
+		for shape, def in pairs(lib_shapes.registered_shapes) do
+			lib_shapes.shapes_created = lib_shapes.shapes_created + 1
+		end
+
+	end
+
+
+	lib_shapes.register_shape = function(shape_name, shape_def)
 	
-	lib_shapes.registered_shapes[new_shape_name] = shape_def
+		local new_shape_name = ""
+		local new_shape_def = {}
+	
+		new_shape_name = "shape_" .. shape_name
+		new_shape_def = {
+			description = shape_def.description .. " Shape",
+			drawtype = "nodebox",
+			tiles = {
+				"dk_grey_noise.png^align_grid_ypos.png",
+				"dk_grey_noise.png^align_grid_yneg.png",
+				"dk_grey_noise.png^align_grid_xpos.png",
+				"dk_grey_noise.png^align_grid_xneg.png",
+				"dk_grey_noise.png^align_grid_zpos.png",
+				"dk_grey_noise.png^align_grid_zneg.png",
+			},
+			paramtype = "light",
+			paramtype2 = "facedir",
+			connects_to = { "group:lib_shapes_library" },
+			is_ground_content = false,
+			walkable = true,
+			climbable = false,
+			buildable_to = false,
+			groups = { oddly_breakable_by_hand = 3, lib_shapes_library = 3 },
+			node_box = shape_def.node_box,
+			selection_box = shape_def.selection_box,
+			collision_box = shape_def.collision_box,
+	
+			on_place = function(itemstack, placer, pointed_thing)
+				if pointed_thing.type ~= "node" then
+					return itemstack
+				end
+	
+				local p0 = pointed_thing.under
+				local p1 = pointed_thing.above
+				local param2 = 0
+	
+				local placer_pos = placer:getpos()
+				if placer_pos then
+					local dir = {
+						x = p1.x - placer_pos.x,
+						y = p1.y - placer_pos.y,
+						z = p1.z - placer_pos.z
+					}
+					param2 = minetest.dir_to_facedir(dir)
+				end
+	
+				if p0.y-1 == p1.y then
+					param2 = param2 + 20
+					if param2 == 21 then
+						param2 = 23
+					elseif param2 == 23 then
+						param2 = 21
+					end
+				end
+	
+				return minetest.item_place(itemstack, placer, pointed_thing, param2)
+			end,
+		}
+	
+	
+		minetest.register_node("lib_shapes:" .. new_shape_name, new_shape_def)
+		minetest.register_craft({
+			output = new_shape_name .. " 1",
+			recipe = {
+				{ 'lib_shapes:blueprint', 'lib_shapes:blueprint', 'lib_shapes:blueprint' },
+				{ '', 'lib_shapes:blueprint', ''},
+				{ '', '', ''},
+			}
+		})
+		
+		lib_shapes.registered_shapes[new_shape_name] = shape_def
+	
+		lib_shapes.shapes_created = lib_shapes.shapes_created + 1
+	
+	end
 
-	lib_shapes.shapes_created = lib_shapes.shapes_created + 1
 
-	minetest.log("NEW SHAPE:     = " .. new_shape_name)
-	--lib_shapes.register_shaped_nodes(shape_name, shape_def)
-
-end
+	lib_shapes.register_material = function(mat_name, shape_set)
+		mat_list[mat_name] = shape_set
+	end
 
 
-dofile(modpath.."/architecture.lua")
-dofile(modpath.."/doors.lua")
-dofile(modpath.."/fences.lua")
-dofile(modpath.."/furniture.lua")
-dofile(modpath.."/general.lua")
-dofile(modpath.."/lights.lua")
+	dofile(lib_shapes.path_mod .. "/lib_shapes_set_fancy.lua")
+	dofile(lib_shapes.path_mod .. "/lib_shapes_register_set_fancy.lua")
+
+	dofile(lib_shapes.path_mod.."/lib_shapes_set_basic.lua")
+	dofile(lib_shapes.path_mod .. "/lib_shapes_register_set_basic.lua")
+
+	dofile(lib_shapes.path_mod.."/lib_shapes_set_doors.lua")
+	dofile(lib_shapes.path_mod .. "/lib_shapes_register_set_doors.lua")
+
+	dofile(lib_shapes.path_mod.."/lib_shapes_set_fences.lua")
+
+	dofile(lib_shapes.path_mod.."/lib_shapes_set_furniture.lua")
+	dofile(lib_shapes.path_mod .. "/lib_shapes_register_set_furniture.lua")
+
+	dofile(lib_shapes.path_mod.."/lib_shapes_set_general.lua")
+
+	dofile(lib_shapes.path_mod.."/lib_shapes_set_lights.lua")
+
+	lib_shapes.get_shapes()
+
+
+
+
+minetest.log(S("[MOD] lib_shapes:  Successfully loaded. Using " .. lib_shapes.shapes_created .. " shapes."))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
